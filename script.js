d52e99ab930dcfabb492016db2fac02d42a4a28c
WillisSz <willisdev5@gmail.com>
2022-03-25 17:14:06 -0300

    script de comparação com numbers do tipo numbers e string

---
+++ script.js
@@ -0,0 +1,26 @@
+let numeroUm = 1 //number
+let stringUm = '1' //string
+
+let numeroTrinta = 30 //number
+let stringTrinta = '30' //string
+
+let numeroDez = 10 //number
+let stringDez = '10' //string
+
+if (numeroUm == stringUm) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+if (numeroTrinta === stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
